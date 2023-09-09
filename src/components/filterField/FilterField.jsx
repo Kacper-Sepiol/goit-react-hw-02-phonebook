9920b250fa89abb344024b1632142337a3b1f2d9
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-09-09 18:40:17 +0200

    update

---
+++ src/components/filterField/FilterField.jsx
@@ -0,0 +1,8 @@
+const FilterField = () => (
+  <>
+    <h2>Find contacts by name</h2>
+    <input type="text" name="filter"></input>
+  </>
+);
+
+export default FilterField;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import FormPhoneBook from './components/formPhoneBook/FormPhoneBook';
 import ListContacts from 'components/listContacts/ListContacts';
+import FilterField from 'components/filterField/FilterField';
 import { nanoid } from 'nanoid';
 
 class PhoneBook extends React.Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     name: '',
     number: '',
+    filter: '',
   };
 
   handleChange = event => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
+  };
+
+  handleChangeFilterField = evt => {
+    this.setState({ filter: evt.currentTarget.value });
   };
 
   handleSubmit = e => {
