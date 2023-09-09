9b70ecb79faee59d070eb8e8cd30426e0d52a039
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-09-09 11:28:34 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class PhoneBook extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>PhoneBook</h1>
+
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<PhoneBook />);
